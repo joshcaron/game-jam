f4306fe3d2c7126b92b6e7e83c45598169a6f83d
Josh Caron <jcaron@ccs.neu.edu>
2012-11-17 20:26:47 -0500

    added project

---
+++ Game Jam/src/objects/Entity.java
@@ -0,0 +1,14 @@
+package objects;
+
+public abstract class Entity {
+	protected int posX;
+	protected int posY;
+	
+	public int getX() {
+		return this.posX;
+	}
+	
+	public int getY() {
+		return this.posY;
+	}
+}
---
+++ Game Jam/src/objects/Prop.java
@@ -0,0 +1,5 @@
+package objects;
+
+public class Prop extends Entity {
+	public Prop() { }
+}
